028daee51a61d774a0efb2db8d74fdd291b87eda
GuillaumeValette <guillaumevalette91@gmail.com>
2020-07-20 16:07:23 +0200

    "Format change for loading Resnet data"

--- dataloaders/HMResnet.py
+++ dataloaders/HMResnet.py
@@ -1,30 +1,27 @@
-from torch.utils.data import Dataset
 import pandas as pd
 import numpy as np
 import os
 import torch
-from utils.utils_loader import data_transform_rn152, img_name_to_PIL_img
+from torch.utils.data import Dataset
+from utils.utils_loader import data_transform_rn152
+from PIL import Image
 
 
 class HMResnet(Dataset):
-    """ Hateful Memes Dataset."""
+    """ Hateful Memes Dataset for Resnet."""
 
-    def __init__(self, name, args, data_path="HMDataset/data"):
+    def __init__(self, name, args, data_path="data"):
 
         assert name in ["train", "dev", "test"]
 
         self.args = args
         self.name = name
 
-        self.file_path = os.path.join(data_path, name + ".jsonl")
-        self.df = pd.read_json(self.file_path, lines=True)
+        self.file_path = os.path.join(data_path, name + "_data")
+        self.df = pd.read_pickle(self.file_path)
 
-        # TODO changer ici en un gros np.load(), appliquer transform une fois pour toute sur
-        #  chaque entrée de l'array
-        self.df['img'] = self.df['img'].map(lambda img_name:
-                                            img_name_to_PIL_img(img_name,
-                                                                data_path,
-                                                                data_transform_rn152))
+        # Transform np.array img to PIL img and apply transform for rn152
+        self.df['img'] = self.df['img'].map(lambda img: data_transform_rn152(Image.fromarray(img)))
 
     def __len__(self):
         return self.df.shape[0]
--- utils/utils_loader.py
+++ utils/utils_loader.py
@@ -1,7 +1,5 @@
-from torchvision import transforms
-import numpy as np
-import os
 from PIL import Image
+from torchvision import transforms
 
 data_transform_rn152 = transforms.Compose([
         transforms.CenterCrop((224, 224)),
@@ -9,12 +7,3 @@ data_transform_rn152 = transforms.Compose([
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
 ])
-
-def img_name_to_PIL_img(img_name, img_dir, transform=None):
-  img_name = img_name.replace('img', 'img_reduced') + '.npy'
-  img_path = os.path.join(img_dir, img_name)
-  arr = np.load(img_path)
-  img = Image.fromarray(arr)
-  if transform:
-    img = transform(img)
-  return img

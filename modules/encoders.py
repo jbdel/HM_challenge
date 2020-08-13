301b5cf7a443188653c70ebb3619b216421a3f3a
GuillaumeValette <guillaumevalette91@gmail.com>
2020-08-13 15:54:01 +0200

    Minor modifs

--- modules/encoders.py
+++ modules/encoders.py
@@ -9,13 +9,13 @@ from torch import nn
 class FineTuneFasterRcnnFc7(nn.Module):
     """ Trainable module for the last fc layer of the faster R-CNN, which is to be fine-tuned. """
 
-    def __init__(self, weigths_file, bias_file):
+    def __init__(self, weights_file, bias_file):
         super(FineTuneFasterRcnnFc7, self).__init__()        
     
-        if not os.path.exists(weigths_file) or not os.path.exists(bias_file):
+        if not os.path.exists(weights_file) or not os.path.exists(bias_file):
             raise FileNotFoundError
 
-        with open(weigths_file, "rb") as w:
+        with open(weights_file, "rb") as w:
             weights = pickle.load(w)
         with open(bias_file, "rb") as b:
             bias = pickle.load(b)
@@ -27,8 +27,8 @@ class FineTuneFasterRcnnFc7(nn.Module):
         self.out_features= bias.shape[0]
 
         self.fc7 = nn.Linear(self.in_features, self.out_features)
-        self.fc7.weight.data.copy_(torch.from_numpy(weights))
-        self.fc7.bias.data.copy_(torch.from_numpy(bias))
+        self.fc7.weight = nn.Parameter(torch.tensor(weights, dtype=torch.float), requires_grad=True)
+        self.fc7.bias = nn.Parameter(torch.tensor(bias, dtype=torch.float), requires_grad=True)
     
     def forward(self, img_features):
         output = nn.functional.relu(self.fc7(img_features))
